00504ab26fda53022b3eff181b889a3763b1549d
Raquel Illerias <raquel.illerias@gmail.com>
2024-10-22 16:58:20 +0100

    Use the Math.abs method to calculate the absolute value of the daysRemaining variable, ensuring that any negative number is converted into a positive one.

--- stage-1/script.js
+++ stage-1/script.js
@@ -1,14 +1,17 @@
 const uiDataElements = [
 	['2024-12-25', 'my-element'],
 	['2025-01-01', 'my-element2'],
+	['2024-02-14', 'my-element3'],
 ];
 
 function getDaysRemaining(date, id) {
 	const millisecondsPerDay = 1000 * 60 * 60 * 24;
 
-	const daysRemaining = Math.round(
-		(new Date(date).setHours(0, 0, 0, 0) - new Date().setHours(0, 0, 0, 0)) /
-			millisecondsPerDay
+	const daysRemaining = Math.abs(
+		Math.round(
+			(new Date(date).setHours(0, 0, 0, 0) - new Date().setHours(0, 0, 0, 0)) /
+				millisecondsPerDay
+		)
 	);
 
 	return (document.getElementById(id).innerText = daysRemaining);
