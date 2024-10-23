2ac355f3b02e299466fc4a12b473991bdb6b99b6
Raquel Illerias <raquel.illerias@gmail.com>
2024-10-23 10:11:12 +0100

    Refactor code to dynamically render the countdown to the target date, ensuring that once the date has passed, the year automatically updates to the next one.

--- task/script.js
+++ task/script.js
@@ -1,22 +1,26 @@
 const uiDataElements = [
-	['2024-12-25', 'my-element'],
-	['2025-01-01', 'my-element2'],
-	['2024-02-14', 'my-element3'],
+	['12-25', 'my-element'],
+	['01-01', 'my-element2'],
+	['02-14', 'my-element3'],
 ];
 
-function calculateDaysDifference(date, id) {
+function getDaysRemaining(date, id) {
+	const currentDate = new Date();
+	const targetDate = new Date(`${currentDate.getFullYear()}-${date}`);
 	const millisecondsPerDay = 1000 * 60 * 60 * 24;
 
-	const daysDifference = Math.abs(
-		Math.round(
-			(new Date(date).setHours(0, 0, 0, 0) - new Date().setHours(0, 0, 0, 0)) /
-				millisecondsPerDay
-		)
+	if (currentDate > targetDate) {
+		targetDate.setFullYear(currentDate.getFullYear() + 1);
+	}
+
+	const daysRemaining = Math.round(
+		(targetDate.setHours(0, 0, 0, 0) - currentDate.setHours(0, 0, 0, 0)) /
+			millisecondsPerDay
 	);
 
-	return (document.getElementById(id).innerText = daysDifference);
+	return (document.getElementById(id).innerText = daysRemaining);
 }
 
 uiDataElements.forEach((dataElement) => {
-	calculateDaysDifference(dataElement[0], dataElement[1]);
+	getDaysRemaining(dataElement[0], dataElement[1]);
 });
