f1ff422d4adc5380a788cbe01f4a8e2506d27f74
Raquel Illerias <raquel.illerias@gmail.com>
2024-10-24 20:38:58 +0100

    Center the accordions for different media queries.

--- task/js/toggleAccordion.js
+++ task/js/toggleAccordion.js
@@ -3,6 +3,8 @@ const toggleAccordions = document.querySelectorAll('.toggleAccordion');
 toggleAccordions.forEach((toggleAccordion) => {
 	toggleAccordion.addEventListener('click', function () {
 		const openedAccordion = this.nextElementSibling;
+		const rotatedArrow = this.lastElementChild;
 		openedAccordion.classList.toggle('active');
+		rotatedArrow.classList.toggle('active');
 	});
 });
