281e8ae7fa169bff524c4a2e7c59d71cfc9b2866
Raquel Illerias <raquel.illerias@gmail.com>
2024-10-24 14:24:26 +0100

    Remove any console.log from the code.

--- task/js/toggleAccordion.js
+++ task/js/toggleAccordion.js
@@ -2,6 +2,5 @@ const toggleAccordion = document.getElementById('toggleAccordion');
 const openedAccordion = document.getElementById('openedAccordion');
 
 toggleAccordion.addEventListener('click', () => {
-	console.log('hi');
 	openedAccordion.classList.toggle('active');
 });
